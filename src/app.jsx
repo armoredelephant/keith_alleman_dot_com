d0608ed4ef7d65380c3bdbecf901d4c3fc3f6e72
Keith Alleman <keith.alleman@icloud.com>
2019-03-05 18:50:01 -0700

    Initial build

---
+++ src/app.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+import './sass/base'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="main">
+                Starter
+            </div>
+        );
+    }
+}
+
+export default App;

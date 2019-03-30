2a3bb5dfdd168b3b63220744d325085d5a5d9abf
Keith Alleman <keith.alleman@icloud.com>
2019-03-30 16:51:26 -0600

    Committing before changing my fixed-nav effect

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import AboutPage from './components/Sections/About';
-import Footer from './components/Shared/Footer';
-import Header from './components/Shared/Header'
-import Landing from './components/HomePage/Landing';
-import ProjectsPage from './components/Sections/Projects';
-import WorkPage from './components/Sections/Work';
+import AboutPage from './components/Sections/About.jsx';
+import Footer from './components/Shared/Footer.jsx';
+import HeaderO from './components/_organisms/00-header/HeaderO.jsx';
+// import HeaderO from './components/_unused/HeaderO';
+import Landing from './components/HomePage/Landing.jsx';
+import ProjectsPage from './components/Sections/Projects.jsx';
+import WorkPage from './components/Sections/Work.jsx';
+
+import ScrollToTop from './components/_unused/ScrollToTop.jsx';
 
 import './sass/base.scss'
 
@@ -14,14 +17,14 @@ class App extends React.Component {
     render() {
         return (
             <Router>
-                <React.Fragment>
-                    <Header />
+                <ScrollToTop>
+                    <HeaderO />
                     <Route exact path='/' component={Landing} />
                     <Route exact path='/about'component={AboutPage} />
                     <Route exact path='/projects' component={ProjectsPage} />
                     <Route exact path='/work' component={WorkPage} />
                     <Footer />
-                </React.Fragment>
+                </ScrollToTop>
             </Router>
         );
     }
